7a4d5cc99c8caa31de3fde3dff2313c259d8c836
Aban-01 <ak0211972@gmail.com>
2025-02-23 10:57:13 +0000

    Area of Circle program

---
+++ Basic/ArCircle.js
@@ -0,0 +1,9 @@
+const pi=3.14
+// const keyword make the value of the variable Fixed forever.
+let r=5
+// let is a keyword used to initiallize a variable in JS. 
+var r2=5*5
+//var keyword does the same thing as let but it is old way and it is not recommended because it causes scope problem.
+let result=pi*r2
+console.log(result)
+console.table([pi,r,result])
