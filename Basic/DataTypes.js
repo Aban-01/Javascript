34290ac032c95e570116dc923849969e3a530dc6
Aban-01 <ak0211972@gmail.com>
2025-02-24 18:46:09 +0000

    Datatypes and script

---
+++ Basic/DataTypes.js
@@ -0,0 +1,35 @@
+// SCRIPT:-
+//ECMAscript -> tc39.es
+//MDN
+
+"use strict";// treat all JS code as newer version
+
+//alert(3+3) we are using nodes js, not browser
+
+//PRIMITIVE DATATYPES:-
+
+//number => 2 to the power 53
+//bigint
+//string =>""
+//symbol => for uniqueness
+//undefined
+//Boolean => true/false
+//null =>standalone value
+
+//NON PRIMITIVE DATATYPE:-
+//object
+
+//Exapmles
+let s="Aban"
+let a=3
+let b=true
+
+console.log(typeof(null)) //object
+console.log(typeof(undefined));  //undefined
+console.log(typeof(s));
+console.log(typeof(a));
+console.log(typeof(b));
+
+
+
+
