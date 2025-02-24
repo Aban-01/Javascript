79c887cdcf97580f82d0f7b3132e73d21fc00e7a
Aban-01 <ak0211972@gmail.com>
2025-02-24 19:06:15 +0000

    Conversion

---
+++ Basic/Conversion.js
@@ -0,0 +1,34 @@
+let a="33"
+let b =null
+
+let aInNumber=Number(a)
+let bInNumber=Number(b)
+
+console.log(typeof a);
+console.log(typeof aInNumber);
+console.log(typeof b);
+console.log(typeof bInNumber);
+
+//"33" => 33
+// "33abc" => NaN ->Not a Number
+//true => 1, false =>0
+
+let s="Srting"
+let sInBoolean=Boolean(s)
+
+console.log(typeof s); // true
+console.log(typeof sInBoolean);
+
+// ""=> false
+// 1=> true, 0=>false
+
+let num=33
+let numInString=String(num)
+
+console.log(typeof num);
+console.log(typeof numInString);
+
+
+
+
+
